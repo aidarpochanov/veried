f7f741c133c9c789a596ec0bd4dfccb6d8cad2d9
aidarpochanov <aidar.pochanov15@imperial.ac.uk>
2018-05-17 19:42:21 +0100

    netlist visible

--- src/Renderer/Renderer.fs
+++ src/Renderer/Renderer.fs
@@ -6,4 +6,30 @@ open Fable.Import
 open Electron
 open Node.Exports
 open Fable.PowerPack
+open Yosys
+open Description
 
+Init().loadViz
+
+let ysReady() = 
+    Browser.document.getElementById("popup").style.visibility <- "hidden"
+    Browser.document.getElementById("popupmsg").style.visibility <- "Please wait..."
+
+
+let synth() = 
+    let work() = 
+        Synthesizer().writeFile("input.v", "code")
+        Synthesizer().run
+        Synthesizer().getSvg
+        Browser.document.getElementById("popup").style.visibility <- "hidden"
+    Browser.document.getElementById("popup").style.visibility <- "visible"
+    Synthesizer().setTimeout
+
+let ys = Synthesizer().ys
+
+ys?verbose <- true
+ys?echo <- true
+
+previewBtn.addEventListener_click(fun _ ->
+    synth()
+)
